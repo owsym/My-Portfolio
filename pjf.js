40051d9e983dd0b994856dfd583aff0a1157c3e6
Usama Farooq <108088072+Usamaf999@users.noreply.github.com>
2022-08-19 06:43:37 -0700

    My Portfolio

---
+++ pjf.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active'); 
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+    document.body.classList.add('active');
+    }else{
+    document.body.classList.remove('active');
+    }
+}
